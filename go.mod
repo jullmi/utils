8f973fa6ef05b339e7d62c12d3905094345bb996
Julia Savchenko <iusavchenko@dinord.ru>
2023-11-16 21:28:01 +0400

    changed func name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/jullmi/utils
+module github.com/jullmi/utils/v2
 
 go 1.21.4
--- utils.go
+++ utils.go
@@ -1,7 +1,7 @@
 package utils
 
 
-func Contains(a []string, x string) bool {
+func InSlice(a []string, x string) bool {
   for _, n := range a {
      if x == n {
         return true
